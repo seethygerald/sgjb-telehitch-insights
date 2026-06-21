114e3e07bf54afe54c5619314b965378f9bc309c
seethygerald <seethygerald@gmail.com>
2026-06-22 06:00:13 +0800

    Update map tiles and request counts

--- frontend/components/TelehitchMap.tsx
+++ frontend/components/TelehitchMap.tsx
@@ -7,7 +7,7 @@ import "leaflet/dist/leaflet.css";
 import { TelehitchRequest } from "../lib/types";
 import { NodeDetails, RequestNode, NodeKind } from "../lib/mapNodes";
 
-const TILE_URL = "https://www.onemap.gov.sg/maps/tiles/Night/{z}/{x}/{y}.png";
+const TILE_URL = "https://www.onemap.gov.sg/maps/tiles/GreyLite/{z}/{x}/{y}.png";
 const TILE_ATTRIBUTION =
   '<img src="https://www.onemap.gov.sg/web-assets/images/logo/om_logo.png" style="height:20px;width:20px;"/>&nbsp;<a href="https://www.onemap.gov.sg/" target="_blank" rel="noopener noreferrer">OneMap</a>&nbsp;&copy;&nbsp;contributors&nbsp;&#124;&nbsp;<a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a>';
 
